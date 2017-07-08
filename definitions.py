cc44a7a9f0d4286e6c84d59d2bf19f27e025a9b6
GaurwaithMelui <strategy155@gmail.com>
2017-07-08 11:45:55 +0300

    changed base api address because urljoin requirements added matches method changed requests urljoin to urllib urljoin

--- definitions.py
+++ definitions.py
@@ -1,5 +1,8 @@
-import os
+import requests
+import urllib
 
-BASE_API_SERVER = "https://api.opendota.com/api"
+BASE_API_SERVER = "https://api.opendota.com/api/"
 PRO_MATCHES_METHOD = "proMatches"
-PRO_MATCHES_REQUEST = os.path.join(BASE_API_SERVER, PRO_MATCHES_METHOD)
+MATCHES_METHOD = "matches"
+PRO_MATCHES_REQUEST = urllib.parse.urljoin(BASE_API_SERVER, PRO_MATCHES_METHOD)
+MATCHES_REQUEST = urllib.parse.urljoin(BASE_API_SERVER, MATCHES_METHOD)
