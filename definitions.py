bbc73d35a20fe2cf40392fbcbf63fefaa1f77c6d
gaurwaithmelui <strategy155@gmail.com>
2017-07-23 17:12:59 +0300

    added plan string entries, added team method\request, added json unserialisation, testing teams method behaviour

--- definitions.py
+++ definitions.py
@@ -4,5 +4,7 @@ BASE_API_SERVER = "https://api.opendota.com/api/"
 PRO_MATCHES_METHOD = "proMatches"
 MATCHES_METHOD = "matches"
 PLAYERS_METHOD = "players"
+TEAMS_METHOD = "teams"
 PRO_MATCHES_REQUEST = urllib.parse.urljoin(BASE_API_SERVER, PRO_MATCHES_METHOD)
 MATCHES_REQUEST = urllib.parse.urljoin(BASE_API_SERVER, MATCHES_METHOD)
+TEAMS_REQUEST = urllib.parse.urljoin(BASE_API_SERVER, TEAMS_METHOD)
--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 import requests
 import definitions
+import json
 
 def get_pro_matches():
-    requests_object = requests.get(definitions.PRO_MATCHES_REQUEST)
-    requests_dictionary = requests_object.text
-    print(requests_dictionary)
+    requests_object = requests.get(definitions.TEAMS_METHOD)
+    requests_dictionary_text = requests_object.text
+    requests_dictionary_unjsoned = json.loads(requests_dictionary_text)
+    print(requests_dictionary_unjsoned[0])
 
 if __name__ == '__main__':
     get_pro_matches()
