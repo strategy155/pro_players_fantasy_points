116a0b812978d93082014d5171e25504f4802671
gaurwaithmelui <strategy155@gmail.com>
2017-07-23 17:28:18 +0300

    added proPlayers method, added ti7 team list changed test format

--- definitions.py
+++ definitions.py
@@ -5,6 +5,15 @@ PRO_MATCHES_METHOD = "proMatches"
 MATCHES_METHOD = "matches"
 PLAYERS_METHOD = "players"
 TEAMS_METHOD = "teams"
+PRO_PLAYERS_METHOD = "proPlayers"
 PRO_MATCHES_REQUEST = urllib.parse.urljoin(BASE_API_SERVER, PRO_MATCHES_METHOD)
 MATCHES_REQUEST = urllib.parse.urljoin(BASE_API_SERVER, MATCHES_METHOD)
 TEAMS_REQUEST = urllib.parse.urljoin(BASE_API_SERVER, TEAMS_METHOD)
+PRO_PLAYERS_REQUEST = urllib.parse.urljoin(BASE_API_SERVER, PRO_PLAYERS_METHOD)
+TEAM_LIST = [
+    "OG",
+    "VP",
+    "EG",
+    "Liquid",
+    ""
+]
--- test.py
+++ test.py
@@ -3,10 +3,10 @@ import definitions
 import json
 
 def get_pro_matches():
-    requests_object = requests.get(definitions.TEAMS_METHOD)
+    requests_object = requests.get(definitions.TEAMS_REQUEST)
     requests_dictionary_text = requests_object.text
     requests_dictionary_unjsoned = json.loads(requests_dictionary_text)
-    print(requests_dictionary_unjsoned[0])
+    print(requests_dictionary_unjsoned)
 
 if __name__ == '__main__':
     get_pro_matches()
