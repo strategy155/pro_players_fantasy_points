bc504b738c4b4be6f1f94f7ada354b66f489796f
gaurwaithmelui <strategy155@gmail.com>
2017-07-23 17:02:14 +0300

    cleanup

--- definitions.py
+++ definitions.py
@@ -1,8 +1,8 @@
-import requests
 import urllib
 
 BASE_API_SERVER = "https://api.opendota.com/api/"
 PRO_MATCHES_METHOD = "proMatches"
 MATCHES_METHOD = "matches"
+PLAYERS_METHOD = "players"
 PRO_MATCHES_REQUEST = urllib.parse.urljoin(BASE_API_SERVER, PRO_MATCHES_METHOD)
 MATCHES_REQUEST = urllib.parse.urljoin(BASE_API_SERVER, MATCHES_METHOD)
