b352fd063ed256aabdad715228aeba586c2f2bfb
gaurwaithmelui <strategy155@gmail.com>
2017-07-30 17:42:35 +0300

    added real tiem getter

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import requests
 import definitions
 import json
-
+import time
 
 def get_pro_players_to_tag(pro_players_request):
     ti_players = {}
@@ -16,6 +16,11 @@ def get_pro_players_to_tag(pro_players_request):
     return ti_players
 
 
+def get_real_time(epoch_time_string):
+    time_str = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(epoch_time_string))
+    return time_str
+
+
 def get_request(api_method):
     requests_object = requests.get(api_method)
     requests_dictionary_text = requests_object.text
@@ -25,4 +30,6 @@ def get_request(api_method):
 
 if __name__ == '__main__':
     pro_players_request = get_request(definitions.PRO_PLAYERS_REQUEST)
+    pro_matches_request = get_request(definitions.PRO_MATCHES_REQUEST)
     ti_players = get_pro_players_to_tag(pro_players_request)
+    print(get_real_time(pro_matches_request[-1]['start_time']))
